739fbfa488149c7ec57a6dcacc1a60bcd371658f
a345560812 <254777938@qq.com>
2019-11-15 23:59:13 +0800

    19.11.15 xiaozhou leetcode[188,198]

---
+++ src/main/java/com/xiaozhou/dp/188.卖股票的最佳时机Ⅳ.java
@@ -0,0 +1,46 @@
+package com.xiaozhou.dp;
+/*
+Say you have an array for which the i-th element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete at most k transactions.
+
+Note:
+You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+
+Example 1:
+
+Input: [2,4,1], k = 2
+Output: 2
+Explanation: Buy on day 1 (price = 2) and sell on day 2 (price = 4), profit = 4-2 = 2.
+Example 2:
+
+Input: [3,2,6,5,0,3], k = 2
+Output: 7
+Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4.
+             Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+思路：这道题考虑两种情况 如果k 大于等于数组的长度一半，那么能保证获得最大利润， 就退化成之前的一道题了，如果小于k.就如下
+tmpMax是当前时间状态持股之后的最大收益（很可能是负的） t[i][j]是记录的实际收益。
+ */
+class Solution188 {
+    public int maxProfit(int k, int[] prices) {
+        int len = prices.length;
+        if (k >= len / 2) return quickSolve(prices);
+        int[][] t = new int[k + 1][len];
+        for (int i = 1; i <= k; i++) {
+            int tmpMax =  -prices[0];
+            for (int j = 1; j < len; j++) {
+                t[i][j] = Math.max(t[i][j - 1], prices[j] + tmpMax);
+                tmpMax =  Math.max(tmpMax, t[i - 1][j - 1] - prices[j]);
+            }
+        }
+        return t[k][len - 1];
+    }
+
+    private int quickSolve(int[] prices) {
+        int len = prices.length, profit = 0;
+        for (int i = 1; i < len; i++)
+            // as long as there is a price gap, we gain a profit.
+            if (prices[i] > prices[i - 1]) profit += prices[i] - prices[i - 1];
+        return profit;
+    }
+}
